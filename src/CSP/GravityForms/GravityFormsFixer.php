4ebb000d7539ed45bd7d82f9789b05fe7435eadb
Robert Bossaert <contact@robertbossaert.com>
2023-08-29 11:58:07 +0200

    (feat): extract onclick handlers to separate script tag

--- src/CSP/GravityForms/GravityFormsFixer.php
+++ src/CSP/GravityForms/GravityFormsFixer.php
@@ -4,6 +4,40 @@ namespace Yard\CSP\GravityForms;
 
 class GravityFormsFixer
 {
+    public function generateUniqueEventListenerName(): string
+    {
+        // Generate a unique name using a combination of timestamp and random number
+        return 'eventListener_' . time() . '_' . mt_rand(1000, 9999);
+    }
+
+    public function handleOnclickAttributes(string $formString): string
+    {
+        $pattern = '/\sonclick\s*=\s*([\'"])((?>\\\\\1|[^\1])*?)\1/m';
+        $scriptTags = [];
+    
+        $formString = preg_replace_callback(
+            $pattern,
+            function (array $matches) use (&$scriptTags): string {
+                $uniqueListenerName = $this->generateUniqueEventListenerName();
+    
+                $onclickCode = html_entity_decode($matches[2]);
+                $scriptTag = '<script id="' . $uniqueListenerName . '">
+                    document.querySelector("[data-onclick-handler=' . $uniqueListenerName . ']").addEventListener("click", function() {
+                        ' . $onclickCode . '
+                    });
+                    </script>';
+    
+                $scriptTags[] = $scriptTag;
+    
+                return ' data-onclick-handler="' . $uniqueListenerName . '"';
+            },
+            $formString
+        );
+    
+        // Append all the script tags to the modified $formString
+        return $formString . implode('', $scriptTags);
+    }
+
     public function handleStyleAttribute(string $formString, array $form): string
     {
         $pattern = '/\sstyle\s*=\s*([\'"])((?>\\\\\1|[^\1])*?)\1/m';
@@ -23,5 +57,13 @@ class GravityFormsFixer
             },
             $formString
         );
+    }
+
+    public function modifyFormHtml(string $formString, array $form): string
+    {
+        $formString = $this->handleOnclickAttributes($formString);
+        $formString = $this->handleStyleAttribute($formString, $form);
+
+        return $formString;
     }
 }
--- src/CSP/GravityForms/GravityFormsServiceProvider.php
+++ src/CSP/GravityForms/GravityFormsServiceProvider.php
@@ -10,7 +10,7 @@ class GravityFormsServiceProvider extends ServiceProvider
     {
         $gravityFormsFixer = new GravityFormsFixer();
 
-        $this->plugin->loader->addFilter('gform_get_form_filter', $gravityFormsFixer, 'handleStyleAttribute', 10, 2);
+        $this->plugin->loader->addFilter('gform_get_form_filter', $gravityFormsFixer, 'modifyFormHtml', 10, 2);
         $this->plugin->loader->addAction('gform_enqueue_scripts', $this, 'enqueueFixerScript', 10, 0);
     }
 
