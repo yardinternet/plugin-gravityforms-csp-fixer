7bf0d652b1332d683908a851ae4f36df09bc9cd6
Maarten Bruna <14947039+ictbeheer@users.noreply.github.com>
2024-08-15 08:19:13 +0200

    (fix) fix js race condition

--- resources/js/csp-fixer.js
+++ resources/js/csp-fixer.js
@@ -12,40 +12,42 @@ jQuery( document ).on(
 );
 
 //Add event handlers for the delete button of uploaded files
-gform.addFilter(
-	'gform_file_upload_markup',
-	function ( html, file, up, strings, imagesUrl, response ) {
-		const regexp = /\son([a-z]*)\s*=\s*([\'"])((>\\\\\2|[^\2])*?)\2/g;
-		const formId = up.settings.multipart_params.form_id;
-		const fieldId = up.settings.multipart_params.field_id;
+jQuery(document).on('gform_post_render', function () {
+	gform.addFilter(
+		'gform_file_upload_markup',
+		function ( html, file, up, strings, imagesUrl, response ) {
+			const regexp = /\son([a-z]*)\s*=\s*([\'"])((>\\\\\2|[^\2])*?)\2/g;
+			const formId = up.settings.multipart_params.form_id;
+			const fieldId = up.settings.multipart_params.field_id;
 
-		const eventHandlers = [ ...html.matchAll( regexp ) ];
-		eventHandlers.forEach( function ( e ) {
-			const eventType = e[ 1 ];
-			up.bind( 'UploadComplete', function () {
-				document
-					.querySelector(
-						'[data-on' +
-							eventType +
-							'-handler=upload' +
-							eventType +
-							'handler_' +
-							formId +
-							'_' +
-							fieldId +
-							']'
-					)
-					.addEventListener( eventType, function () {
-						gformDeleteUploadedFile( formId, fieldId, this );
-					} );
+			const eventHandlers = [ ...html.matchAll( regexp ) ];
+			eventHandlers.forEach( function ( e ) {
+				const eventType = e[ 1 ];
+				up.bind( 'UploadComplete', function () {
+					document
+						.querySelector(
+							'[data-on' +
+								eventType +
+								'-handler=upload' +
+								eventType +
+								'handler_' +
+								formId +
+								'_' +
+								fieldId +
+								']'
+						)
+						.addEventListener( eventType, function () {
+							gformDeleteUploadedFile( formId, fieldId, this );
+						} );
+				} );
 			} );
-		} );
 
-		html = html.replaceAll(
-			regexp,
-			' data-on$1-handler="upload$1handler_' + formId + '_' + fieldId + '"'
-		);
+			html = html.replaceAll(
+				regexp,
+				' data-on$1-handler="upload$1handler_' + formId + '_' + fieldId + '"'
+			);
 
-		return html;
-	}
-);
+			return html;
+		}
+	);
+});
