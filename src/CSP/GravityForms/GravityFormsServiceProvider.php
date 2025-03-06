de329021027f898a62d63fdee51e7cea2821cd8e
Maarten Bruna <14947039+ictbeheer@users.noreply.github.com>
2025-03-06 13:25:29 +0100

    (feat): allow multi uploads again

--- src/CSP/GravityForms/GravityFormsServiceProvider.php
+++ src/CSP/GravityForms/GravityFormsServiceProvider.php
@@ -12,28 +12,10 @@ class GravityFormsServiceProvider extends ServiceProvider
 
         $this->plugin->loader->addFilter('gform_get_form_filter', $gravityFormsFixer, 'modifyFormHtml', 10, 2);
         $this->plugin->loader->addAction('gform_enqueue_scripts', $this, 'enqueueFixerScript', 10, 0);
-        $this->plugin->loader->addAction('admin_enqueue_scripts', $this, 'disableMultiFileUpload', 11, 0);
     }
 
     public function enqueueFixerScript(): void
     {
         wp_enqueue_script('csp-fixer', sprintf('%s/resources/js/csp-fixer.js', $this->plugin->getPluginUrl()), ['jquery'], $this->plugin->getVersion());
-    }
-
-    public function disableMultiFileUpload()
-    {
-        $inlineScript = "
-            gform.addFilter( 'gform_editor_field_settings', function( settings, field ) {
-                if ( field.type !== 'fileupload' ) {
-                    return settings;
-                }
-                return settings.filter((setting) => setting !== '.multiple_files_setting');
-            } );";
-
-        wp_add_inline_script(
-            'gform_form_admin',
-            $inlineScript,
-            'before'
-        );
     }
 }
