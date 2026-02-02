6e99bf42c024a9144e35ae801dfe2c5326bacc4f
rivanuff <rivanuff@gmail.com>
2026-02-02 14:59:57 +0100

    chore: up version

--- gravityforms-csp-fixer.php
+++ gravityforms-csp-fixer.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name:       GravityForms CSP Fixer
  * Description:       Fixes GravityForms plugin, so it is CSP compliant.
- * Version:           0.1.0
+ * Version:           0.1.2
  * Requires at least: 5.9
  * Requires PHP:      7.4
  * Author:            Yard | Digital Agency
@@ -17,7 +17,7 @@ if (! defined('WPINC')) {
     die;
 }
 
-define('YCSP_VERSION', '0.1.0');
+define('YCSP_VERSION', '0.1.2');
 define('YCSP_DIR', basename(__DIR__));
 define('YCSP_ROOT_PATH', __DIR__);
 
